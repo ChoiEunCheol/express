e8369e3738f5c3e5c4f0450ce60fbbd60c0fd462
최은철 <wndqhrgkdlel@gmail.com>
2023-10-26 11:08:31 +0900

    GPT를 활용하여 express 모듈을 사용한 서버 구축

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.json()); // JSON 데이터 파싱
+app.use(express.urlencoded({ extended: true })); // URL-encoded 데이터 파싱
+
+// 정적 파일 제공을 위한 미들웨어 설정
+app.use(express.static(__dirname));
+
+// GET 요청을 처리
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname });
+});
+
+// POST 요청을 처리
+app.post('/submit', (req, res) => {
+  const data = req.body.inputData; // "inputData"는 입력 필드의 이름입니다
+  console.log('POST 요청을 받았습니다:', data);
+  res.send('POST 요청을 받았습니다. 입력한 내용: ' + data);
+});
+
+// 서버를 8000번 포트에서 시작
+app.listen(port, () => {
+  console.log(`서버가 ${port} 포트에서 실행 중입니다.`);
+});
